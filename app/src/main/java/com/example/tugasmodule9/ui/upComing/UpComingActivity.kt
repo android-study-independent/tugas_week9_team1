9e798edb43809a735a26aa5d8f8758d767f9b1d2
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-10-15 21:10:07 +0700

    commit sementara

--- app/src/main/java/com/example/tugasmodule9/ui/upComing/UpComingActivity.kt
+++ app/src/main/java/com/example/tugasmodule9/ui/upComing/UpComingActivity.kt
@@ -2,6 +2,8 @@ package com.example.tugasmodule9.ui.upComing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.tugasmodule9.R
 import com.example.tugasmodule9.data.api.Network
@@ -11,6 +13,21 @@ class UpComingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_up_coming)
+
+        lifecycleScope.launch {
+
+            val result = Network.getService(this@UpComingActivity).getUpcoming(
+                page = 1
+            )
+
+            Log.d("debug", "total page -> ${result.totalPage}")
+
+
+            result.results.map {
+                Log.d("debug", "hasilnya -> ${it.title} - ${it.overview}")
+            }
+
+        }
         
     }
 }
