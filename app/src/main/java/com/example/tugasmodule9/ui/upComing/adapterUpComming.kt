8e61aab145a477232c0089ffb3b41cba78dc5edb
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-10-16 21:35:51 +0700

    fitur laman menampilkan data Up Comming Berhasil

--- app/src/main/java/com/example/tugasmodule9/ui/upComing/adapterUpComming.kt
+++ app/src/main/java/com/example/tugasmodule9/ui/upComing/adapterUpComming.kt
@@ -10,7 +10,8 @@ import com.example.tugasmodule9.R
 import com.example.tugasmodule9.data.response.MovieResponse
 import com.squareup.picasso.Picasso
 
-class adapterUpComming(private var listMovie: List<MovieResponse>): RecyclerView.Adapter<adapterUpComming.UpCommingHolder>() {
+class adapterUpComming(private val listMovie: List<MovieResponse>):
+    RecyclerView.Adapter<adapterUpComming.UpCommingHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UpCommingHolder {
         val view = LayoutInflater.from(parent.context)
