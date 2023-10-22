97b50c6c66b5822aeeabc8645f02ab30450bda8b
Mhmdazhis <abdulazis101101@yahoo.com>
2023-10-22 22:41:01 +0700

    mengubah sebagian file dan menambahkan file adapter dan response

--- app/src/main/java/com/example/tugasmodule9/data/response/PopulerResponse.kt
+++ app/src/main/java/com/example/tugasmodule9/data/response/PopulerResponse.kt
@@ -3,17 +3,10 @@ package com.example.tugasmodule9.data.response
 import com.google.gson.annotations.SerializedName
 
 data class PopulerResponse(
-
-
-	val page: Int? = null,
-	val results: List<PopulerResponse?>? = null,
-
-	@field:SerializedName("title")
-	val title: String? = null,
-
-	@field:SerializedName("poster_path")
-	val posterPath: String? = null,
-
-
+	val page: Int,
+	val results: List<MovieResponse>,
+	@SerializedName("total_page")
+	val totalPage: Int,
+	@SerializedName("total_result")
+	val totalResult: Int,
 )
-
